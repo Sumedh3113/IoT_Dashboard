35637f3cf9add1d0994217253d361890bea62142
Sumedh3113 <sumedhdeshpande31@gmail.com>
2018-11-17 00:37:32 -0800

    date fetch files

--- iot/js/date_fetch.js
+++ iot/js/date_fetch.js
@@ -1,20 +1,20 @@
 $(document).ready(function(){
 //making an ajax call to followersdata.php file
   $.ajax({
-    url : "http://localhost/iot/date_fetch.php",
+    url : "http://localhost/iot/display_date.php",
     type : "GET",
     success : function(data){
       console.log(data);
 
       var userid = [];
       var facebook_follower = [];
-	  var facebook_follower2 = [];
+	 // var facebook_follower2 = [];
      
 
       for(var i in data) {
-        userid.push("UserID " + data[i].sequence);
-        facebook_follower.push(data[i].value);
-		facebook_follower2.push(data[i].value2)
+        userid.push("UserID " + data[i].date);
+        facebook_follower.push(data[i].Lights);
+		//facebook_follower2.push(data[i].value2)
           }
 
       var chartdata = {
@@ -29,16 +29,6 @@ $(document).ready(function(){
             pointHoverBackgroundColor: "rgba(59, 89, 152, 1)",
             pointHoverBorderColor: "rgba(59, 89, 152, 1)",
             data: facebook_follower
-          },
-          {
-            label: "sensor2",
-            fill: false,
-            lineTension: 0.1,
-            backgroundColor: "rgba(29, 202, 255, 0.75)",
-            borderColor: "rgba(29, 202, 255, 1)",
-            pointHoverBackgroundColor: "rgba(29, 202, 255, 1)",
-            pointHoverBorderColor: "rgba(29, 202, 255, 1)",
-            data: facebook_follower2
           }
           
         ]
