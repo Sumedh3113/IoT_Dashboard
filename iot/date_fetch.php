abb606f14e565cd096fb1384416ae7067b130cd1
Sumedh3113 <sumedhdeshpande31@gmail.com>
2018-11-17 00:35:34 -0800

    date fetch files

--- iot/date_fetch.php
+++ iot/date_fetch.php
@@ -2,19 +2,35 @@
 header('Content-Type: application/json');
 
 require_once('connect.php');
+
      $from = $_GET["from"];
 	 $to = $_GET["to"];
-	$sql = "SELECT * FROM `sensordata` WHERE time BETWEEN '$from' AND '$to'";
-	$result = mysqli_query($connection, $sql) or die (mysql_error ());
+	// $f = '45';
+	// $t = '65';
+	//echo $from."  \n";
+	//echo  $to ; 
+	
+	
+	$sql = "SELECT * FROM final WHERE date BETWEEN '$from' AND '$to'";
+//	"SELECT * FROM final WHERE Lights BETWEEN '" . $from . "' AND  '" . $to . "'";
+	
+	//
+	
+	$result= mysqli_query($connection, $sql);
+    //	or die (mysql_error ());
+	
 	
 	$data = array();
-
+	
 	foreach ($result as $row) {
 	$data[] = $row;
 	}
-
-	// while ($row = mysqli_fetch_array ($result)){
 	
+	
+
+	//On page 1
+	session_start();
+	$_SESSION['varname'] = $data;
 	//printing these values is needed to generate the graph
 	print json_encode($data);
 	
